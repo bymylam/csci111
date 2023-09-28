c4f3fe91f38642d71d68c3cd9c35de7eb1590c99
mylam233 <107911641+mylam233@users.noreply.github.com>
2023-09-28 12:08:40 -0400

    Add files via upload

---
+++ MyLamAssignment4.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class MyLamAssignment4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+
+        //Food Menu
+        double Pizza = 15;
+        String Pizza$ = formatter.format(Pizza);
+        double Salad = 8;
+        String Salad$ = formatter.format(Salad);
+        double Sandwich = 9;
+        String Sandwich$ = formatter.format(Sandwich);
+        double Pasta = 10;
+        String Pasta$ = formatter.format(Pasta);
+        double Soup = 7;
+        String Soup$ = formatter.format(Soup);
+
+        System.out.println("Welcome to the Restaurant");
+        System.out.println("Here is our menu:");
+        System.out.println("Pizza: " + Pizza$);
+        System.out.println("Salad: " + Salad$);
+        System.out.println("Sandwich: " + Sandwich$);
+        System.out.println("Pasta: " + Pasta$);
+        System.out.println("Soup: " + Soup$);
+
+        System.out.println("Which item would you like?");
+        String item = scan.nextLine();
+
+        double food_price = 0;
+        if (item.equals("Pizza")) {
+            food_price += Pizza;
+        } else if (item.equals("Salad")) {
+            food_price += Salad;
+        } else if (item.equals("Sandwich")) {
+            food_price += Sandwich;
+        } else if (item.equals("Pasta")) {
+            food_price += Pasta;
+        } else if (item.equals("Soup")) {
+            food_price += Soup;
+        } else {
+            System.out.println("Invalid response, try again later.");
+            System.exit(0);
+        }
+
+        //---------------------------------------------------------------
+        //Drink Menu
+
+        double Soda = 2;
+        String Soda$ = formatter.format(Soda);
+        double Coffee = 3;
+        String Coffee$ = formatter.format(Coffee);
+
+        System.out.println("Would you like to add a Soda for $2 or a Coffee for $3?");
+        System.out.println("Enter [S] for soda, [C] for coffee or [N] for neither.");
+        String drink = scan.next();
+
+        double drink_price = 0;
+        if (drink.equals("S")) {
+            drink_price += Soda;
+        } else if (drink.equals("C")) {
+            drink_price += Coffee;
+        } else if (drink.equals("N")) {
+            drink_price += 0;
+        } else {
+            System.out.println("Invalid response, try again later.");
+            System.exit(0);
+        }
+
+        //--------------------------------------------------------------
+        //SUBTOTAL
+        double Subtotal = food_price + drink_price;
+        String Subtotal$ = formatter.format(Subtotal);
+        System.out.println("Subtotal: " + Subtotal$);
+
+        //--------------------------------------------------------------
+        //TAX
+        double tax = Subtotal*0.06;
+        String tax$ = formatter.format(tax);
+        System.out.println("Tax: " + tax$);
+
+        //--------------------------------------------------------------
+        //TOTAL
+        double total = Subtotal + tax;
+        String total$ = formatter.format(total);
+        System.out.println("Tax : " + total$);
+
+    }
+}
