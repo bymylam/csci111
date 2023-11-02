ac35f9f0970a2ab4a23ffa090be16cb619a36d66
mylam233 <107911641+mylam233@users.noreply.github.com>
2023-11-01 22:37:49 -0400

    Update MyLamAssignment6.java
    still print error

--- MyLamAssignment6.java
+++ MyLamAssignment6.java
@@ -2,46 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class MyLamAssignment6 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws FileNotFoundException {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("How many random numbers do you want to print out into the file?");
+//        int HowManyRand = scan.nextInt();
+
         //insert random numbers into a file
-        Random rand = new Random();
+//        Random rand = new Random();
         String inputName = "input.txt";
-        PrintWriter createInput = new PrintWriter(inputName);
-        for (int i = 1; i <= 10; i++) {
-            int number = rand.nextInt(6);
-            createInput.println(number);
-        }
-        createInput.close();
+//        PrintWriter createInput = new PrintWriter(inputName);
+//        for (int i = 1; i <= 10; i++) {
+//            int number = rand.nextInt();
+//            createInput.println(number);
+//        }
+//        createInput.close();
 
         //open file
         File file = new File(inputName);
         Scanner input = new Scanner(file);
 
         //SUM & COUNT
-        int count = 0;
-        int sum = 0;
-        while (input.hasNextInt()) {
+        int count = 1;
+        double min = input.nextDouble();
+        double max = min;
+        double sum = min;
+        double val;
+        while (input.hasNextDouble()) {
+            val = input.nextDouble();
+            //COUNT
             count++;
-            double x = input.nextInt();
-            sum += x;
-        }
-
-        //MIN & MAX
-        int val  = 0;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        while (input.hasNextInt()) {
-            val = input.nextInt();
-            if (val > max) { //max
-                max = val;
-            }
-            if (val < min) { //min
+            //SUM
+            sum+=val;
+            //MIN
+            if (min > val) {
                 min = val;
             }
+            //MAX
+            if (max < val) {
+                max = val;
+            }
+            input.close();
         }
 
         //AVERAGE
-        double average = sum/count;
+        double average = (double) sum/count;
 
         String outputName = "output.txt";
         PrintWriter createOutput = new PrintWriter(outputName);
