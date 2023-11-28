64eb79e7cafa801ff6e252b23648ed2883f28fbe
mylam233 <107911641+mylam233@users.noreply.github.com>
2023-11-28 12:01:21 -0500

    OOP stuff

---
+++ Point.java
@@ -0,0 +1,27 @@
+class Point {
+    //FIELDS
+    int x;
+    int y;
+
+    //CONSTRUCTORS
+    public Point() {
+        x = 0; y = 0;
+    }
+        //allow to initialize directly the fields at the Point object
+    public Point(int initialX, int initialY) {
+        x = initialX;
+        y = initialY;
+    }
+
+    //BEHAVIOR
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+
+    //MUTATOR METHODS
+    public void se
+
+}
---
+++ pointMain.java
@@ -0,0 +1,14 @@
+public class pointMain {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        Point p2 = new Point();
+
+        //accessing fields
+        //x & y here are fields
+        p1.x = 3;
+        p2.y = 4;
+
+        System.out.println(p1.x + ", " + p2.y);
+
+    }
+}
