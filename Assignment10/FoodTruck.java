f7fbd5817649543bcf41151eb8076cfe33947304
mylam233 <107911641+mylam233@users.noreply.github.com>
2023-12-06 19:13:59 -0500

    Update FoodTruck.java

--- Assignment10/FoodTruck.java
+++ Assignment10/FoodTruck.java
@@ -1,134 +1,87 @@
-import java.text.NumberFormat;
-/**
-* This class implements a Food Truck with the following menu:
-* - Coffee
-* - Soda
-* - Bagel
-* - Sandwich
-* - Chips
-*/
-public class FoodTruck {
-    /**
-    * Class Constants:
-    * These are static, final values - meaning they will remain
-    * unchanged for any instance of the FoodTruck class
-    */
+/*
+ * This program simulates customer orders from a food truck.
+ *
+ * There are a series of "tests" in which a randomized number of
+ * customers order a randomized amount of menu items from a single
+ * instance of FoodTruck.
+ *
+ * The number of tests that occur are decided by the user at runtime.
+ *
+ * The test outcomes will be printed to a file called output.txt
+ */
 
-    // Beginning inventory of menu items
-    private static final int COFFEE_STARTING_INVENTORY = 200;
-    private static final int SODA_STARTING_INVENTORY = 75;
-    private static final int BAGEL_STARTING_INVENTORY = 40;
-    private static final int SANDWICH_STARTING_INVENTORY = 75;
-    private static final int CHIPS_STARTING_INVENTORY = 75;
-    // Menu item prices
-    public static final double COFFEE_PRICE = 2;
-    public static final double SODA_PRICE = 1.5;
-    public static final double BAGEL_PRICE = 3;
-    public static final double SANDWICH_PRICE = 8;
-    public static final double CHIPS_PRICE = 2;
-    // Tax rate
-    public static final double TAXRATE = 0.06;
-    // Currency formatter
-    private static final NumberFormat FORMATTER = NumberFormat.getCurrencyInstance();
+import java.util.*;
+import java.io.*;
 
-    /**
-    * Class Fields:
-    * These are non-static instance variables
-    * These fields are initialized with a value
-    * in the constructor. The field values may change
-    * throughout the life of the instance.
-    */
+public class FoodTruckMain {
+    public static void main(String[] args) throws IOException {
+        //Scanner and PrintWriter tools
+        Scanner scan = new Scanner(System.in);
+        PrintWriter printer = new PrintWriter(new File("output.txt"));
 
-    // Remaining inventory
-    private int coffeesRemaining;
-    private int sodasRemaining;
-    private int bagelsRemaining;
-    private int sandwichesRemaining;
-    private int chipsRemaining;
+        //User decides the number of tests that will occur
+        System.out.println("Enter number of tests: ");
+        int numTests = scan.nextInt();
 
-    // Order counters
-    private int fulfilledOrders;
-    private int unfulfilledOrders;
+        //Begin printing to Output file:
+        printer.println("TOTAL TESTS COMPLETED: " + numTests + "\n");
 
-    // Gross Revenue
-    private double revenue;
-
-    /**
-    * Constructor
-    */
-    public FoodTruck() {
-        coffeesRemaining = COFFEE_STARTING_INVENTORY;
-        sodasRemaining = SODA_STARTING_INVENTORY;
-        bagelsRemaining = BAGEL_STARTING_INVENTORY;
-        sandwichesRemaining = SANDWICH_STARTING_INVENTORY;
-        chipsRemaining = CHIPS_STARTING_INVENTORY;
-        unfulfilledOrders = 0;
-        fulfilledOrders = 0;
+        //Run tests
+        for(int i = 0; i < numTests; i++) {
+            printer.print("TEST NUMBER " + (i+1) + ": ");
+            //Call the tester method
+            //Pass the file printer as an input argument
+            foodTruckTester(printer);
+        }
+        //Close and save the output file.
+        printer.close();
     }
 
-    /**
-    * Instance methods:
-    * These methods ARE tied to an instance of a class. This means
-    * these methods operate by accessing the state of a specific
-    * instance of FoodTruck.
-    */
-    public String toString() {
-        return "Filled Orders = " + fulfilledOrders + ", Unfulfilled Orders = " + unfulfilledOrders +
-                ", Revenue = " + FORMATTER.format(revenue);
-    }
+    /*
+     * This method will "test" an instance of a FoodTruck:
+     * - Given a newly constructed instance of the FoodTruck class,
+     * this method will generate a randomized number of orders
+     * with a randomized number of menu items per order
+     */
+    public static void foodTruckTester(PrintWriter printer) throws IOException{
+        //Instantiating a Random object here
+        //We will use this to randomly generate the number of customers
+        //We will also use this to randomly generate each customer's order
+        Random rand = new Random();
 
-    //TO DO: Implement this method
-    public void order(int numCoffees, int numSodas, int numBagels,
-            int numSandwiches, int numChips) {
-        // This method will:
-        // - Checks the current inventory to see if all items are available
-        // - IF the entire order is available:
-        
-            // - deduct the ordered items from remaining inventory
-            // - increment the fulfilledOrders field
-            // - add the cost of the order to the revenue field
-            // (cost is the menu item price * the menu item count)
-        // - IF there is not sufficient inventory to fulfill order:
-            // - increment the unfulfilledOrders field
-            // - leave remaining inventory and revenue unchanged
-    }
+        //Here, we instantiate new new FoodTruck object.
+        //Each FoodTruck starts out with a standard inventory,
+        //which you can view in the FoodTruck class.
+        FoodTruck truck = new FoodTruck();
 
-    /**
-    * Static Methods:
-    * These are methods that are not tied to a specific instance.
-    * These are public methods, therefore they can be used internally
-    * and externally in client code.
-    */
-    // This method will return a formatted String representing a receipt.
-    public static String formatReceipt(double subtotal) {
-        double tax = subtotal * TAXRATE;
-        double total = subtotal + tax;
-        int space = 12;
-    return String.format("%-" + space + "s", "Subtotal:") +
-            FORMATTER.format(subtotal) + "\n" +
-            String.format("%-" + space + "s", "Tax:") + FORMATTER.format(tax) +
-            "\n" +
-            String.format("%-" + space + "s", "Total:") +
-            FORMATTER.format(total) + "\n";
-    }
+        //Determines the maximum number of customers
+        //that will place orders with this food truck
+        int maxNumCustomers = 50;
+        int numCustomers = rand.nextInt(maxNumCustomers) + 1;
 
-    //This method will return a mes sage stating the order cannot be fulfilled.
-    public static String declineOrderMessage() {
-        return "Unfortunately, we cannot fulfill your order. " + "\nHave a nice day!";
-    }
+        //Determines the boundary for random item count
+        //in each order
+        int randItemBound = 4;
 
-    //This method will return the menu
-    public static String menu() {
-        int space = 12;
-        return String.format("%-" + space + "s", "Coffee:") +
-            FORMATTER.format(COFFEE_PRICE) + "\n" +
-            String.format("%-" + space + "s", "Soda:") +
-            FORMATTER.format(SODA_PRICE) + "\n" +
-            String.format("%-" + space + "s", "Bagel:") +
-            FORMATTER.format(BAGEL_PRICE) + "\n" +
-            String.format("%-" + space + "s", "Sandwich:") +
-            FORMATTER.format(SANDWICH_PRICE) + "\n" +
-            String.format("%-" + space + "s", "Chips:") +
-            FORMATTER.format(CHIPS_PRICE) + "\n";
+        //Start printing infomation about this specific test
+        printer.println("Customer Count = " + numCustomers);
+
+        for(int i = 0; i < numCustomers; i++) {
+            //This represents a randomized customer order
+            //The customer will order a random number of coffees, sodas, bagels, etc.
+            //Keep in mind that we need to call rand.nextInt each time we want
+            //a new random number.
+            //If we just called rand.nextInt once, it means that the count of coffees, sodas, etc
+            //would all be the same.
+            truck.order(rand.nextInt(randItemBound),
+                    rand.nextInt(randItemBound),
+                    rand.nextInt(randItemBound),
+                    rand.nextInt(randItemBound),
+                    rand.nextInt(randItemBound));
+        }
+
+        //Invoke the toString method for the FoodTruck object
+        printer.println(truck);
+        printer.println();
     }
 }
